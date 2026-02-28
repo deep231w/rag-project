453dc962ca369866c0cdd69284c000568bcc2e02
Deepak Maharana <100968930+deep231w@users.noreply.github.com>
2026-02-28 15:06:47 +0530

    delete api router fixed

--- src/routes/deleteCollection.ts
+++ src/routes/deleteCollection.ts
@@ -3,9 +3,15 @@ import { qdrant } from "../lib/qdrant";
 
 export const router = Router();
 
-router.delete('/',async(res:Response, req:Request)=>{
+router.delete('/',async( req:Request ,res:Response)=>{
     try{
         const {collectionName}=req.body;
+        console.log("collection name-", collectionName);
+        if(!collectionName){
+            res.status(400).json({message:"please enter collection name"});
+            return;
+        }
+
         const response =await qdrant.deleteCollection(`${collectionName}`)
         console.log("delete collection route response- ", response);
 
