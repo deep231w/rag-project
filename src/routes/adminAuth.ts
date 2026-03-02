f5fa4e9fa074c5bbc935b73644c01dba6f96a19b
Deepak Maharana <100968930+deep231w@users.noreply.github.com>
2026-03-02 10:46:03 +0530

    added signup route for admin

---
+++ src/routes/adminAuth.ts
@@ -0,0 +1,44 @@
+import { Request, Response, Router } from "express";
+import bcrypt from 'bcryptjs';
+import { Admin } from "../model/adminSchema";
+
+const route= Router();
+
+route.post("/signup", async(req:Request, res:Response)=>{
+    try{
+        const {name ,email , password}=req.body;
+
+        if(!name || !email || !password){
+            res.status(400).json({message:"missing credentials!"});
+            return;
+        }
+
+        const hashedPassword= await bcrypt.hash(password , 12);
+
+        const existingUser= await Admin.findOne({email});
+
+        if(existingUser){
+            res.status(403).json({message:"user with same gmail already exist , please try different email"});
+            return;
+        }
+
+        const admin= await Admin.create({
+            name,
+            email,
+            password:hashedPassword
+        })
+
+        // if(!admin){
+        //     res.status(403).json({message:"admin not created"})
+        // }
+
+        res.status(200).json({
+            message:"admin created",
+            admin:admin
+        })
+        
+    }catch(e){
+        console.log("error in admin signup page: ", e);
+        throw e
+    }
+})
