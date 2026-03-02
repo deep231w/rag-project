dfcd9e2794a12b95916ae577361b88cd4dfbcd23
Deepak Maharana <100968930+deep231w@users.noreply.github.com>
2026-03-02 11:24:56 +0530

    updated admin signup page

--- src/routes/adminAuth.ts
+++ src/routes/adminAuth.ts
@@ -2,6 +2,7 @@ import { Request, Response, Router } from "express";
 import bcrypt from 'bcryptjs';
 import { Admin } from "../model/adminSchema";
 import jwt from 'jsonwebtoken';
+import { env } from "../config/env";
 
 export const route= Router();
 
@@ -29,14 +30,27 @@ route.post("/signup", async(req:Request, res:Response)=>{
             password:hashedPassword
         })
 
-        // const token;
+        const payload={
+            userId:admin._id,
+            email:admin.email,
+            name:admin.name
+        }
+
+        if (!env.MY_JWT_SECRET) {
+            throw new Error("MY_JWT_SECRET is missing");
+        }
+
+
+        const token=jwt.sign(payload , env.MY_JWT_SECRET  , {expiresIn:'1h'});
+
         // if(!admin){
         //     res.status(403).json({message:"admin not created"})
         // }
 
         res.status(200).json({
             message:"admin created",
-            admin:admin
+            admin:payload,
+            token:token
         })
 
     }catch(e){
