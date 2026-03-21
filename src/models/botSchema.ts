ce682b913b5979815db88813724508251704220f
Deepak Maharana <100968930+deep231w@users.noreply.github.com>
2026-03-21 07:38:51 +0530

    update db schema added files array for bot

--- src/models/botSchema.ts
+++ src/models/botSchema.ts
@@ -4,6 +4,14 @@ const botSchema= new mongoose.Schema(
     {
         name:{type:String , required:true},
         adminId:{type:String, required:true},
+        files:[
+            {
+                name:{type:String , require:true},
+                url:{type:String ,require:true},
+                type:String,
+                uploadedAt:{type:Date ,default :Date.now()}
+            }
+        ]
     },
     {timestamps:true}
 )
