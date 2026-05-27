fcf9081e4176573eb5a1c7077d2810237894d39a
Deepak Maharana <100968930+deep231w@users.noreply.github.com>
2026-05-27 22:49:57 +0530

    updated embedded provider

--- src/providers/GetEmbeddedProvider.ts
+++ src/providers/GetEmbeddedProvider.ts
@@ -1,10 +1,35 @@
 import { EmbeddedProvider } from "./EmbeddedProvider";
+import { GoogleGeminiEmbedded } from "./embedding/gemini";
 import { OllamaEmbedded } from "./embedding/ollama";
+import { OpenAIEmbeddingProvider } from "./embedding/openAI";
 
-export function GetEmbeddedProvider(type:string):EmbeddedProvider{
+interface ProviderConfig {
+  apiKey?: string;
+  model?: string;
+}
+
+export function GetEmbeddedProvider(type:string , config:ProviderConfig):EmbeddedProvider{
     switch(type){
         case "ollama":
             return new OllamaEmbedded();
+        
+        case "openai":
+            if(! config.apiKey || !config.model){
+                throw new Error("API config require")
+            }
+            return new OpenAIEmbeddingProvider({
+                apiKey:config.apiKey,
+                model:config.model
+            })
+
+        case "gemini":
+            if(! config.apiKey || !config.model){
+                throw new Error("API config require")
+            }
+            return new GoogleGeminiEmbedded({
+                apiKey:config.apiKey,
+                model:config.model
+            })
      
         default:
             throw new Error("Unsupported provider");
