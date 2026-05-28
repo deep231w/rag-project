6afca2d67f3a3f350f80a3ac276e48979fd223fe
Deepak Maharana <100968930+deep231w@users.noreply.github.com>
2026-05-28 23:54:00 +0530

    update:(insgentchunk updated)

--- src/services/ingent.service.ts
+++ src/services/ingent.service.ts
@@ -2,6 +2,9 @@ import { qdrant } from "../lib/qdrant";
 import { embededText } from "../lib/embeding";
 import OllamaLocalEmbded from "../lib/OllamaLocalEmbed";
 import { chunckText } from "../lib/chunck";
+import { ApiProviderConfig } from "../models/apiProviderConfig";
+import { GetEmbeddedProvider } from "../providers/GetEmbeddedProvider";
+import { env } from "../config/env";
 export async function ingestChunk(
   botId:string,
   adminId:string,
@@ -13,8 +16,33 @@ export async function ingestChunk(
     try{
       const chunckedText= chunckText(text);
 
+      //getting embedded config from db by admin id
+
+      const embeddedConfig= await ApiProviderConfig.findOne({adminId});
+
       let vectors:number[][];
-      vectors = await OllamaLocalEmbded(chunckedText);
+
+      //condition base embedded provider call:
+      const isProd = process.env.NODE_ENV === "production";
+
+      if(embeddedConfig?.embeddedProvider && embeddedConfig.embeddedApiKey && embeddedConfig.embeddedModel){
+        vectors =await GetEmbeddedProvider(
+          embeddedConfig.embeddedProvider ,{
+            apiKey:embeddedConfig.embeddedApiKey,
+            model:embeddedConfig.embeddedModel
+          }
+        ).generateEmbeddings(chunckedText)
+
+      }else if(!isProd){
+        vectors = await OllamaLocalEmbded(chunckedText);
+      }else{
+        vectors= await GetEmbeddedProvider(
+          "gemini",{
+            apiKey:env.EMBEDDING_API_KEY,
+            model:env.EMBEDDING_MODEL
+          }).generateEmbeddings(chunckedText)
+      }
+
       console.log(vectors.length);
       console.log(vectors[0].length);
 
